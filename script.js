666444542fd0e2fe9c3b59862f3fbd16f1cdffe3
Angie <agladinop@gmail.com>
2021-12-19 21:32:54 -0500

    styles:banner, about, projects, contact, footer. Form with javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+/* $ as prefix when it is a DOM element */
+
+const $form = document.querySelector("#contact-form")
+const $mailTo = document.querySelector("#mail-to")
+$form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault() /* Avoid reloading the page */
+    const form = new FormData(this)
+    $mailTo.setAttribute("href", `mailto:agladinop@gmail.com?subject=${form.get("name")} - ${form.get("subject")} &body= ${form.get("email")} - ${form.get("message")}`)
+    $mailTo.click()
+}
