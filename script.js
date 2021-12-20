bb5171f8cb8c9de7c3540eef8266b7fc7269f325
Angie <agladinop@gmail.com>
2021-12-20 13:46:25 -0500

    header finished, working links on project cards

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 /* $ as prefix when it is a DOM element */
 
+/* header responsive */
+const body = document.querySelector("body");
+const nav = document.querySelector(".nav");
+const menu = document.querySelector(".nav__menu");
+const menuButton = document.querySelector(".menu__icon--hamburger");
+const closeButton = document.querySelector(".menu__icon--close");
+
+menuButton.onclick = () => {
+    menu.classList.add("nav__menu--active");
+    menuButton.classList.add("hide");
+    closeButton.classList.add("show");
+    body.classList.add("disabledScroll");
+}
+closeButton.onclick = () => {
+    menu.classList.remove("nav__menu--active");
+    menuButton.classList.remove("hide");
+    closeButton.classList.remove("show");
+    body.classList.remove("disabledScroll");
+}
+
+window.onscroll = () => {
+    this.scrollY > 20 ? nav.classList.add("sticky") : nav.classList.remove("sticky");
+}
+
+
+/* Form */
 const $form = document.querySelector("#contact-form")
 const $mailTo = document.querySelector("#mail-to")
 $form.addEventListener("submit", handleSubmit)
